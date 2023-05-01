8ce4e9f7d515c3b01f41b35bd4b765716c3cdc9b
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2023-05-01 20:56:48 +0530

    working v15 added

--- app.js
+++ app.js
@@ -1,53 +1,35 @@
-#!/usr/bin/env node
-
 const express = require('express')
 const app = express()
 const port = 8080
-const stockfish = require("stockfish");
-const engine = stockfish();
+const loadEngine = require("./stockfishjs/example/load_engine.js");
+let engine = loadEngine(require("path").join(__dirname, "/stockfishjs/src/stockfish.js"));
+const mode = "EVAL"
+
+// seems wrong
 const fenregex = "/^([rnbqkpRNBQKP1-8]+\/){7}([rnbqkpRNBQKP1-8]+)\s[bw]\s(-|K?Q?k?q?)\s(-|[a-h][36])\s(0|[1-9][0-9]*)\s([1-9][0-9]*)/"
-
-engine.onmessage = function(msg) {
-  console.log(msg);
-};
-
-engine.postMessage("uci");
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.post('/', (request, response) => {
-
-  // if (!request.body.fen.match(fenregex)) {
-  //   console.log(request.body.fen)
-  //   response.send("Invalid fen string");
-  //   return;
-  // }
-
-// if chess engine replies
-  engine.onmessage = function(msg) {
-    console.log({msg});
-    // in case the response has already been sent?
-    if (response.headersSent) {
-        return;
-    }
-    // only send response when it is a recommendation
-    if (typeof(msg == "string") && msg.match("bestmove")) {
-        response.send(msg);
-    }
-  }
+  console.log("time start", Date.now())
 
 // run chess engine
-  engine.postMessage("ucinewgame");
-  engine.postMessage("position fen " + request.body.fen);
-  engine.postMessage("eval")
-  // engine.postMessage("go depth 18");
+  engine.send("ucinewgame");
+  engine.send("position fen " + request.body.fen);
+  engine.send(mode === "EVAL" ? "eval" : "go depth 18", function onDone(result){
+    console.log({result})
+    response.status(200).send(result)
+    // engine.quit();
+  }, function onStream(data){
+    console.log({data})
+  });
+  console.log("time end", Date.now())
 });
 
-app.listen(port, (err) => {
+app.listen(port, async (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
-
   console.log(`server is listening on ${port}`)
 })
