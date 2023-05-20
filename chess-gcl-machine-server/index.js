3597e280b5da95c01c0b5c803390f0a907366174
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2023-05-20 15:57:05 +0530

    Make it chess-gcl-machine-server multi file

--- chess-gcl-machine-server/index.js
+++ chess-gcl-machine-server/index.js
@@ -1,19 +1,21 @@
 import fs from "fs"
 import fetch from "node-fetch"
 
-const filePath = "./temp.txt"
+const filePaths = ["./temp.txt", "./temp2.txt"]
 
-fs.watchFile(filePath, {interval: 10} ,() => {
-  fs.readFile(filePath, 'utf8', async function(err, data){
-    fetch("http://ec2-43-205-201-239.ap-south-1.compute.amazonaws.com:8080/", {
-      method: "POST",
-      body: JSON.stringify({pgnFile: data}),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then(res => res.json())
-    .then(res => console.log({res}))
-    .catch(err => console.log(err))
-  });
-})
+for (let i = 0; i < filePaths.length; i++) {
+  fs.watchFile(filePaths[i], {interval: 10} ,() => {
+    fs.readFile(filePaths[i], 'utf8', async function(err, data){
+      fetch("http://ec2-43-205-201-239.ap-south-1.compute.amazonaws.com:8080/", {
+        method: "POST",
+        body: JSON.stringify({pgnFile: data}),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then(res => res.json())
+      .then(res => console.log({res}))
+      .catch(err => console.log(err))
+    });
+  })
+}
