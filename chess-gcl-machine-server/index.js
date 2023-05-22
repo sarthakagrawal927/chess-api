56e5a6665ab09ba0dfe3dda4f91852ba89ba0afe
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2023-05-22 22:43:28 +0530

    Update script for local machine

--- chess-gcl-machine-server/index.js
+++ chess-gcl-machine-server/index.js
@@ -1,21 +1,26 @@
 import fs from "fs"
 import fetch from "node-fetch"
 
-const filePaths = ["./temp.txt", "./temp2.txt"]
+const filePaths = ["./temp.txt"]
+const API_ENDPOINT = "ec2-15-206-69-32.ap-south-1.compute.amazonaws.com"
 
 for (let i = 0; i < filePaths.length; i++) {
   fs.watchFile(filePaths[i], {interval: 10} ,() => {
     fs.readFile(filePaths[i], 'utf8', async function(err, data){
-      fetch("http://ec2-43-205-201-239.ap-south-1.compute.amazonaws.com:8080/", {
-        method: "POST",
-        body: JSON.stringify({pgnFile: data}),
-        headers: {
-          "Content-Type": "application/json"
-        }
-      })
-      .then(res => res.json())
-      .then(res => console.log({res}))
-      .catch(err => console.log(err))
+      try {
+        fetch(API_ENDPOINT, {
+          method: "POST",
+          body: JSON.stringify({pgnFile: data}),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        .then(res => res.json())
+        .then(res => console.log({res}))
+        .catch(err => console.log({err}))
+      }catch(err){
+        console.log({err})
+      }
     });
   })
 }
